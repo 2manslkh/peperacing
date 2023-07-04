040b3042642965b897939836fb637e79f3510e95
Kenk <kenghin_lim@hotmail.com>
2023-07-04 13:16:34 +0900

    feat: add contracts

---
+++ packages/contracts/scripts/deposit.ts
@@ -0,0 +1,23 @@
+import { config, deployments, ethers, hardhatArguments } from 'hardhat';
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const address = await deployer.getAddress();
+
+  const testcontract = await deployments.get('TestContract');
+
+  const contract = await ethers.getContractAt('TestContract', testcontract.address, deployer);
+
+  const deposit = await contract.deposit({ value: ethers.parseEther('1') });
+  console.log("🚀 | main | deposit:", deposit)
+
+  let tx = await deposit.wait();
+  console.log("🚀 | main | tx:", tx)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
