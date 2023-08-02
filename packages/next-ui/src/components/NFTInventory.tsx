c4aabd60422321794adb885782b5d18a2d55a33c
Kenk <kenghin_lim@hotmail.com>
2023-08-02 11:17:39 +0700

    feat(next-ui): update query

--- packages/next-ui/src/components/NFTInventory.tsx
+++ packages/next-ui/src/components/NFTInventory.tsx
@@ -2,27 +2,31 @@
 
 import { useEffect, useState } from 'react'
 
+// import Image from 'next/image'
 import { getNFTs } from '../utils/graph'
 import styled from 'styled-components'
+import { useAccount } from 'wagmi'
 
 const InventoryItemContainer = styled.div`
     display: inline-block;
     margin: 10px;
     border: 1px solid black;
+    border-bottom: 5px solid black;
     padding: 10px;
     border-radius: 10px;
     img {
         width: 50px;
         height: 50px;
+        display: block;
     }
 `
 
 export function NFTInventory() {
-
     const [diamondsList, setDiamondsList] = useState<any>()
+    const { address } = useAccount()
 
     useEffect(() => {
-        getNFTs().then((diamonds) => {
+        getNFTs(address?.toLocaleLowerCase() as `0x${string}`).then((diamonds) => {
             setDiamondsList(diamonds?.data.tokens)
             console.log(diamonds?.data.tokens)
         })
@@ -36,9 +40,8 @@ export function NFTInventory() {
             {diamondsList?.map((diamond: any) => {
                 return (
                     <InventoryItemContainer key={diamond.id} >
-                        <img src={diamond.uri} />
+                        <img src={diamond.uri} alt="diamond" />
                     </InventoryItemContainer>
-
                 )
             }
             )}
--- packages/next-ui/src/utils/graph/queries.ts
+++ packages/next-ui/src/utils/graph/queries.ts
@@ -2,19 +2,19 @@ import { client } from './client'
 import { gql } from '@apollo/client'
 
 const GET_DIAMOND_NFTS = `
-    query DiamondNFTs {
-        tokens {
+    query getNFTsByOwner($owner: String) {
+        tokens (where: {owner: $owner}){
         id
         uri
-      }
+        }
     }
 `
-export async function getNFTs() {
+export async function getNFTs(owner: `0x${string}`) {
     try {
         return await client
             .query({
                 query: gql(GET_DIAMOND_NFTS),
-                variables: {},
+                variables: { owner: owner },
             })
     } catch (error) {
         console.log(error)
